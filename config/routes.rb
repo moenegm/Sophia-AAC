98c94ac6c784dde72d351324f3928a314098a82c
Mohamed A. Negm <mr.negm90@gmail.com>
2022-07-28 21:12:12 -0700

    page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+  get "activities", to: 'posts#activities'
+  get "crafts", to: 'posts#crafts'
+  get "drinks", to: 'posts#drinks'
+  get "feelings", to: 'posts#feelings'
+  get "food", to: 'posts#food'
+  get "people", to: 'posts#people'
+  get "places", to: 'posts#places'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
