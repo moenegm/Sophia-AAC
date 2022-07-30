c3451cd851ecd0546d7713237511eb64d52922d6
Mohamed A. Negm <mr.negm90@gmail.com>
2022-07-30 01:14:59 -0700

    fixed grid, validated post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+    validates :title,:category,:location,:image,:sound, presence: true
+    
+    validates :title, format: { with: /\A[a-zA-Z0-9-_]+\Z/, message: 'Title cannot contain whitespace or special characters' }
+
     has_one_attached :image
     has_one_attached :sound
+
 end
