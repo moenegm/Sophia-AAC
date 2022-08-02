af29e562fbc29a601ea3bda8627f229f57961394
Mohamed A. Negm <mr.negm90@gmail.com>
2022-08-02 16:42:39 -0700

    space fixing

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,10 @@ module PostsHelper
         photo.variant(:gravity=>"Center", resize: "500x500^", crop: "500x500+0+0")
     end
 
+    def title(string)
+        string = string.gsub(" ", "-")
+    end
+
 end
 
 
